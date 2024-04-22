5d16f34e46f5eab7dc30f53abfe59429d1548503
Enrico070 <125027577+Enrico070@users.noreply.github.com>
2024-04-22 11:17:49 -0300

    criação do banco de dados e da tabela

--- db/script.sql
+++ db/script.sql
@@ -1,16 +1,16 @@
 --Criação do banco
 
-CREATE DATABASE aulaback;
+CREATE DATABASE exbb;
 
 --Crição da tabela
 
 CREATE TABLE usuarios(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    email VARCHAR(100) NOT NULL,
+    datanascimento DATE NOT NULL,
+    idade INTEGER NOT NULL,
+    signo VARCHAR(100) NOT NULL
 )
 
---Inserção de dados teste
 
-INSERT INTO usuarios(nome, email) VALUES ('João', 'joaosantos@gmail.com');
-INSERT INTO usuarios(nome, email) VALUES ('M.Rita', 'mariarita@gmail.com');
