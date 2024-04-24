f99d5bb22f93c6b84f5e49f4640459b046d6cc1c
Enrico070 <125027577+Enrico070@users.noreply.github.com>
2024-04-24 14:41:57 -0300

    criação da primeira parte e das funções do signo e idade

--- db/script.sql
+++ db/script.sql
@@ -7,6 +7,7 @@ CREATE DATABASE exbb;
 CREATE TABLE usuarios(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     datanascimento DATE NOT NULL,
     idade INTEGER NOT NULL,
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { Pool } = require('pg')
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
+
 
 const pool = new Pool({
     user: 'postgres',
@@ -14,6 +15,7 @@ const pool = new Pool({
 
 
 app.use(express.json());
+
 
 
 app.get('/usuarios', async (req, res) => {
@@ -30,10 +32,58 @@ app.get('/usuarios', async (req, res) => {
 })
 
 
+
+function calcularIdade(dataNascimento) {
+  const hoje = new Date();
+  let idade = hoje.getFullYear() - dataNascimento.getFullYear();
+  const mesAtual = hoje.getMonth();
+  const mesNascimento = dataNascimento.getMonth();
+  if (mesNascimento > mesAtual || (mesNascimento === mesAtual && hoje.getDate() < dataNascimento.getDate())) {
+    idade--;
+  }
+  return idade;
+}
+
+
+
+function calcularSigno(mes, dia) {
+  if ((mes === 1 && dia >= 20) || (mes === 2 && dia <= 18)) {
+    return 'Aquário';
+  } else if ((mes === 2 && dia >= 19) || (mes === 3 && dia <= 20)) {
+    return 'Peixes';
+  } else if ((mes === 3 && dia >= 21) || (mes === 4 && dia <= 19)) {
+    return 'Áries';
+  } else if ((mes === 4 && dia >= 20) || (mes === 5 && dia <= 20)) {
+    return 'Touro';
+  } else if ((mes === 5 && dia >= 21) || (mes === 6 && dia <= 20)) {
+    return 'Gêmeos';
+  } else if ((mes === 6 && dia >= 21) || (mes === 7 && dia <= 22)) {
+    return 'Câncer';
+  } else if ((mes === 7 && dia >= 23) || (mes === 8 && dia <= 22)) {
+    return 'Leão';
+  } else if ((mes === 8 && dia >= 23) || (mes === 9 && dia <= 22)) {
+    return 'Virgem';
+  } else if ((mes === 9 && dia >= 23) || (mes === 10 && dia <= 22)) {
+    return 'Libra';
+  } else if ((mes === 10 && dia >= 23) || (mes === 11 && dia <= 21)) {
+    return 'Escorpião';
+  } else if ((mes === 11 && dia >= 22) || (mes === 12 && dia <= 21)) {
+    return 'Sagitário';
+  } else {
+    return 'Capricórnio'; 
+  }
+}
+
+
+
 app.post('/usuarios', async (req, res) => {
     try {
-        const { nome, email } = req.body;
-        await pool.query('INSERT INTO usuarios (nome, email) VALUES ($1, $2)', [nome, email])
+        const { nome, sobrenome, email, datadenascimento} = req.body;
+
+        const dataNascimento = new Date(datadenascimento);
+        const idade = calcularIdade(dataNascimento);
+        const signo = calcularSigno(dataNascimento.getMonth() + 1, dataNascimento.getDate());
+        await pool.query('INSERT INTO usuarios (nome, sobrenome, email, datadenascimento, idade, signo) VALUES ($1, $2, $3, $4, $5, $6)', [nome, sobrenome, email, datadenascimento, idade, signo])
         res.status(201).send({ mensagem: 'Usuario criado com sucesso' })
     } catch (error) {
         console.error('Erro ao inserir o usuário', error);
@@ -58,8 +108,13 @@ app.delete('/usuarios/:id', async (req, res) => {
 app.put('/usuarios/:id', async (req, res) => {
     try {
       const { id } = req.params;
-      const { nome, email } = req.body;
-      await pool.query('UPDATE usuarios SET nome = $1, email = $2 WHERE id = $3', [nome, email, id]);
+      const { nome, sobrenome, email, datadenascimento} = req.body;
+      const dataNascimento = new Date(datadenascimento);
+      const idade = calcularIdade(dataNascimento);
+      const signo = calcularSigno(dataNascimento.getMonth() + 1, dataNascimento.getDate());
+
+
+      await pool.query('UPDATE usuarios SET nome = $1, sobrenome = $2, email = $3, datadenascimento = $4, idade = $5, signo = $6 WHERE id = $7', [nome, sobrenome, email, datadenascimento, idade, signo, id]);
       res.status(200).send({ mensagem: 'Usuário atualizado com sucesso'});
     } catch (error) {
       console.error('Erro ao atualizar usuário:', error);
